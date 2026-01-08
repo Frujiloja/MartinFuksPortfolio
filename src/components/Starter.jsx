b86f8405d62a72086e68d96b75a96bf7f0517982
Martin Fuks <martinfukss@gmail.com>
2026-01-08 11:39:19 -0300

    io sono add

--- src/components/Starter.jsx
+++ src/components/Starter.jsx
@@ -31,6 +31,7 @@ const Landing = () => {
     //{ name: "BIZUPPLY", image: "/bizupply.jpg", link: "https://bizupply.com.ar/" },
     //{ name: "VITACRED", image: "/vitacred.jpg", link: "https://vitacred.com.ar/" },
     { name: "INSTITUTO DALET", image: "/dalet.jpg", link: "https://institutodalet.com.ar/" },
+    { name: "IO SONO", image: "/imageee.png", link: "https://iosonoeu.com/" },
     //{ name: "MUTUAL CERRI", image: "/Screenshot 2025-10-29 130905.jpg", link: "https://mutualcerri.com.ar/" },
     { name: "MARTIN PELACH", image: "/imagee.png", link: "https://mpelachprop.com.ar/" },
     { name: "MOLA JEWELRY", image: "/Screenshot 2025-05-29 130159.jpg", link: "https://molajewelry.com/" },
